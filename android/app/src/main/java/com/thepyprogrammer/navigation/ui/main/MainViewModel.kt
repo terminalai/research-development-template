4ba7796cf48ef39f89a8f1c2e9a037cdba4d2a30
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-10-22 15:28:46 +0800

    Add Android Kotlin Template, R&D Template, Vue Template and Streamlit Template

---
+++ android/app/src/main/java/com/thepyprogrammer/navigation/ui/main/MainViewModel.kt
@@ -0,0 +1,15 @@
+package com.thepyprogrammer.navigation.ui.main
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+    var image = MutableLiveData<String>()
+
+    var pName = MutableLiveData("name")
+
+    fun getResultName() = pName
+
+
+}
